607754850b9a82ff0dd7b9505218e2768f264ce1
jalen <jalenparham97@gmail.com>
2018-10-04 13:23:52 -0400

    vue router setup and oauth working

---
+++ src/components/AuthHandler.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Please Wait...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthHandler"
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App";
+import store from "./store";
+import AuthHandler from "./components/AuthHandler";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/oath2/callback", component: AuthHandler }
+  ]
+});
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
